9c1b5ac4f35c2a69338d8a66cb88e492c89822a6
Daniele Conti <fourlastor@users.noreply.github.com>
2023-05-09 09:14:53 +0100

    Remove unneeded dependencies

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -34,11 +34,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
 tasks.test {
     useJUnitPlatform()
 }
