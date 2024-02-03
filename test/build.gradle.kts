89a406d66779938caa9a48a3d082eecb18e1da68
Daniele Conti <fourlastor@users.noreply.github.com>
2024-02-03 11:25:10 +0100

    Remove commented code

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -49,7 +49,6 @@ construo {
     targets {
         create<Target.Linux>("linuxX64") {
             architecture.set(Target.Architecture.X86_64)
-//            jdkUrl.set("https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.8_7.tar.gz")
         }
         create<Target.MacOs>("macX64") {
             architecture.set(Target.Architecture.X86_64)
