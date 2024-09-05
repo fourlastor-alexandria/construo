0e1d2aebf3d5b2be5a17432a5ba736552bd29bef
Daniele Conti <fourlastor@users.noreply.github.com>
2024-09-05 19:19:14 +0200

    Add script to update changelog

---
+++ update_changelog.sh
@@ -0,0 +1,2 @@
+docker run -it --rm -e CHANGELOG_GITHUB_TOKEN=$CHANGELOG_GITHUB_TOKEN -v "$(pwd)":/usr/local/src/your-app githubchangeloggenerator/github-changelog-generator --user fourlastor-alexandria --project construo
+
