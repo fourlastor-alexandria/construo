36f4e55f60ba7f695dc0fa0db8c19399049c4b2e
Daniele Conti <fourlastor@users.noreply.github.com>
2025-02-22 17:22:12 +0100

    Add test files

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -57,6 +57,7 @@ construo {
         }
         create<Target.Windows>("winX64") {
             architecture.set(Target.Architecture.X86_64)
+            icon.set(project.file("blast-ico.png"))
             jdkUrl.set("https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.8_7.zip")
         }
         create<Target.Windows>("winX64NoGpu") {
