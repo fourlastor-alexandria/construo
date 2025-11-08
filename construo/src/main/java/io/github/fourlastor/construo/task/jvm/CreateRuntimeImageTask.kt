9771f3d23b9acb208ca9deccc3bc71be405a261a
Thosea <104597976+imthosea@users.noreply.github.com>
2025-11-07 21:52:13 -0500

    change java verison command
    Co-authored-by: Daniele Conti <fourlastor@users.noreply.github.com>

--- construo/src/main/java/io/github/fourlastor/construo/task/jvm/CreateRuntimeImageTask.kt
+++ construo/src/main/java/io/github/fourlastor/construo/task/jvm/CreateRuntimeImageTask.kt
@@ -118,8 +118,7 @@ abstract class CreateRuntimeImageTask @Inject constructor(
 		jarFile.get().asFile.absolutePath
 	).splitToSequence("\n").map { it.trim() }.filter { it.isNotBlank() }.toList()
 
-	private fun getJvmVersion(javaHome: File): String = executeInJdk(javaHome, "java", "--version")
-		.split(" ")[2].removePrefix("\"").split(".")[0]
+	private fun getJvmVersion(javaHome: File): String = executeInJdk(javaHome, "javap", "-version")
 
 	private fun executeInJdk(javaHome: File, command: String, vararg args: String): String = ByteArrayOutputStream().use {
 		execOperations.exec {
