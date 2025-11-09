56d08f5a90916090c688281b9386f10efd023577
Thosea <104597976+imthosea@users.noreply.github.com>
2025-11-09 17:33:30 -0500

    fix version command

--- construo/src/main/java/io/github/fourlastor/construo/task/jvm/CreateRuntimeImageTask.kt
+++ construo/src/main/java/io/github/fourlastor/construo/task/jvm/CreateRuntimeImageTask.kt
@@ -118,7 +118,7 @@ abstract class CreateRuntimeImageTask @Inject constructor(
 		jarFile.get().asFile.absolutePath
 	).splitToSequence("\n").map { it.trim() }.filter { it.isNotBlank() }.toList()
 
-	private fun getJvmVersion(javaHome: File): String = executeInJdk(javaHome, "javap", "-version")
+	private fun getJvmVersion(javaHome: File): String = executeInJdk(javaHome, "javap", "-version").split(".")[0]
 
 	private fun executeInJdk(javaHome: File, command: String, vararg args: String): String = ByteArrayOutputStream().use {
 		execOperations.exec {
